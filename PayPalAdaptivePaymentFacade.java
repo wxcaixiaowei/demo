1b46bac15f3b2dc0b0d1adba4751c8555a676a20
vaibhavkaul <vaibhavkaul85@gmail.com>
2014-10-21 10:26:29 -0400

    more work for release 1

--- PayPalAdaptivePaymentFacade.java
+++ PayPalAdaptivePaymentFacade.java
@@ -8,7 +8,7 @@ public interface PayPalAdaptivePaymentFacade {
 	PayPalCustomerPaymentDetails pay(String senderEmail, String recieverEmail, String amount, String preApprovalKey,String restrictedPreApprovalKey, Integer orderItemId, Boolean isReissue) throws NeedsPreApprovalException;
 
 	PayPalPreApprovalResponse preapproval(String senderEmail, Date startDate, Date endDate, Double maxTotalPayment, String cancelUrl, String returnUrl, boolean isRestricted);
-
+//and some more changes
 	void updatePaypalAdaptivePaymentStatus(String paymentExecStatus, String transactionId, String transactionStatus, String senderTransactionId, String senderTransactionStatus, Integer orderItemId);
 	
 }
